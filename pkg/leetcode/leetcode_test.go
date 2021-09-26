aa0668e65780174e58cf3468e2bf577ee4dc246f
Brent <brentfreder@gmail.com>
2021-09-26 22:14:09 +0000

    Add unit test to leetcode package

---
+++ pkg/leetcode/leetcode_test.go
@@ -0,0 +1,44 @@
+package leetcode
+
+import "testing"
+
+const testAccount = "brents_smurf_account"
+
+func TestFindIfUserCompletedLeetCodeProblemIsTrue(t *testing.T) {
+	problemName := "two-sum"
+	completed, err := FindIfUserCompletedLeetCodeProblem(testAccount, "two-sum")
+
+	if err != nil {
+		t.Errorf("User: %s, Problem: %s, received error: %v", testAccount, problemName, err)
+	}
+
+	if !completed {
+		t.Errorf("User: %s, Problem: %s, received false, expected true", testAccount, problemName)
+	}
+}
+
+func TestFindIfUserCompletedLeetCodeProblemNotAttempted(t *testing.T) {
+	problemName := "three-sum"
+	completed, err := FindIfUserCompletedLeetCodeProblem(testAccount, "two-sum")
+
+	if err != nil {
+		t.Errorf("User: %s, Problem: %s, received error: %v", testAccount, problemName, err)
+	}
+
+	if completed {
+		t.Errorf("User: %s, Problem: %s, received true, expected false", testAccount, problemName)
+	}
+}
+
+func TestFindIfUserCompletedLeetCodeProblemNotAccepted(t *testing.T) {
+	problemName := "add-two-numbers"
+	completed, err := FindIfUserCompletedLeetCodeProblem(testAccount, "two-sum")
+
+	if err != nil {
+		t.Errorf("User: %s, Problem: %s, received error: %v", testAccount, problemName, err)
+	}
+
+	if !completed {
+		t.Errorf("User: %s, Problem: %s, received true, expected false", testAccount, problemName)
+	}
+}
