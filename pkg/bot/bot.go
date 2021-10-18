45c79fd5481929e3ed1dfeda43847f1988fccd92
Brent <brentfreder@gmail.com>
2021-10-18 22:36:26 +0000

    Bare minimum implementation.
    This just pings and verifies discords interaction based on the headers.
    The only requirements for your bot to be thought of as a bot.

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -1,7 +1,11 @@
 package bot
 
 import (
+	"crypto/ed25519"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"strconv"
@@ -9,9 +13,21 @@ import (
 	"github.com/brfrs/Discord-ACM-Bot-Go/pkg/bot/cmds"
 )
 
-const ENV_APP_ID = "ACM_APP_ID"
-const ENV_TOKEN_ID = "ACM_BOT_TOKEN"
-const ENV_PORT = "ACM_BOT_PORT"
+// Env vars for the app to use
+const (
+	ENV_APP_ID         = "ACM_APP_ID"
+	ENV_APP_PUBLIC_KEY = "ACM_APP_PUBLIC_KEY"
+	ENV_TOKEN_ID       = "ACM_BOT_TOKEN"
+	ENV_PORT           = "ACM_BOT_PORT"
+	ENV_KEY_FILE       = "ACM_BOT_KEY_FILE"
+	ENV_CERT_FILE      = "ACM_BOT_CERT_FILE"
+)
+
+// Private constants for discord interaction signatures
+const (
+	sig_header       = "X-Signature-Ed25519"
+	timestamp_header = "X-Signature-Timestamp"
+)
 
 var basicCmds = []cmds.CmdData{
 	{
@@ -35,17 +51,29 @@ var basicCmds = []cmds.CmdData{
 }
 
 type Bot struct {
-	appId   string
-	token   string
-	started bool
-	port    int
+	appId        string
+	token        string
+	appPublicKey []byte
+	started      bool
+	port         int
 }
 
 func (bot *Bot) New() error {
 	var err error
 	bot.appId = os.Getenv(ENV_APP_ID)
 	bot.token = os.Getenv(ENV_TOKEN_ID)
+	bot.appPublicKey, err = hex.DecodeString(os.Getenv(ENV_APP_PUBLIC_KEY))
+
+	if err != nil {
+		return err
+	}
+
 	bot.port, err = strconv.Atoi(os.Getenv(ENV_PORT))
+
+	if err != nil {
+		return err
+	}
+
 	bot.started = true
 
 	if bot.appId == "" {
@@ -56,11 +84,49 @@ func (bot *Bot) New() error {
 		return fmt.Errorf("missing env var %s", ENV_TOKEN_ID)
 	}
 
+	if len(bot.appPublicKey) != ed25519.PublicKeySize {
+		return fmt.Errorf("bot public key is not ed25519 public key size. Size of key: %d", len(bot.appPublicKey))
+	}
+
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func verifyInteraction(bot *Bot, r *http.Request) (bool, error) {
+	sigEntry := r.Header[sig_header]
+	if sigEntry == nil || len(sigEntry) != 1 {
+		return false, fmt.Errorf("request error: signature header missing or unexpected number of entries.")
+	}
+	sig, err := hex.DecodeString(sigEntry[0])
+
+	if err != nil {
+		return false, err
+	}
+
+	if len(sig) != ed25519.PrivateKeySize {
+		return false, fmt.Errorf("request error: signature size doesn't match ed25519 private key size. Size of key: %d", len(sig))
+	}
+
+	timestampEntry := r.Header[timestamp_header]
+	if timestampEntry == nil || len(timestampEntry) != 1 {
+		return false, fmt.Errorf("request error: timestamp header missing or unexpected number of entries.")
+	}
+	timestamp := timestampEntry[0]
+
+	fmt.Printf("Sig: %s\n", sigEntry[0])
+	fmt.Printf("Timestamp: %s\n", timestamp)
+
+	body, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		return false, err
+	}
+
+	message := append([]byte(timestamp), body...)
+	return ed25519.Verify(bot.appPublicKey, message, sig), nil
 }
 
 func (bot *Bot) InitGuild(guildId string) {
@@ -73,9 +139,32 @@ func (bot *Bot) Serve() error {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("received request")
+		if err := r.ParseForm(); err != nil {
+			fmt.Printf("Error parsing form: %v\n", err)
+			return
+		}
+		fmt.Printf("Request received. Method: %s\n", r.Method)
+		verified, err := verifyInteraction(bot, r)
 
+		if err != nil {
+			fmt.Printf("Error encountered trying to verify interaction: %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if !verified {
+			fmt.Println("Error encountered trying to verify message. Returning 401")
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		encoder := json.NewEncoder(w)
+		responseData := map[string]int{
+			"type": 1,
+		}
+
+		encoder.Encode(responseData)
 	})
 
-	return http.ListenAndServe(fmt.Sprintf(":%d", bot.port), nil)
+	return http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", bot.port), nil)
 }
